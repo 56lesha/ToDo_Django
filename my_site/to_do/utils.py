8b3f6c38a828ec631a67945ed202e9bcd8bb3839
Alexey <56_lesha@mail.ru>
2023-07-26 13:06:22 +0300

    add caching

--- my_site/to_do/utils.py
+++ my_site/to_do/utils.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+
 from to_do.models import Category
 
 menu = [{'title': 'Home', 'url_name': 'show_tasks'},
@@ -9,9 +11,9 @@ class DataMixin:
     paginate_by = 2
     def get_user_context(self, **kwargs):
         context = kwargs
-        context['categories'] = Category.objects.all()
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
             user_menu.pop(1)
         context['menu'] = user_menu
+        context['categories'] = Category.objects.all()
         return context
